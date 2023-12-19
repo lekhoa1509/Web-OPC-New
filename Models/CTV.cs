8b6be1612e9083d97cb28b1760e0509464362eee
Lê Khoa <39689858+lekhoa1509@users.noreply.github.com>
2023-12-19 10:58:08 +0700

    thêm CTV

--- Models/CTV.cs
+++ Models/CTV.cs
@@ -7,16 +7,19 @@ namespace web4.Models
 {
     public class CTV
     {
+        public int Id { get; set; }
+        public List<B30CTVDetail> Details { get; set; }
+
         public DateTime Ngay_Ct { get; set; }
-      public  string So_Ct { get; set; }
+        public  string So_Ct { get; set; }
        public string Ma_Dt { get; set; }
-      public  string Dvcs { get; set; }
+        public  string Dvcs { get; set; }
         public string Loai_TP { get; set; }
         public string Ma_vt { get; set; }
-        public string ten_vt { get; set; }
-
+        public string Ten_Vt { get; set; }
+        public string Ma_Vt_SAP { get; set; }
         public string Dvt { get; set; }
-        public int Han_muc { get; set; }
+        public int Han_Muc { get; set; }
         public string Ten_Dt { get; set; }
     }
 }
